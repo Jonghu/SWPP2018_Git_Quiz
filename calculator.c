1b5cc60a646dc5b40da65d2d13d7b975c2aeb24c
Jonghu <jjonghu@gmail.com>
2018-03-15 17:13:48 +0900

    Implement add, sub, mul : squashed

--- calculator.c
+++ calculator.c
@@ -23,13 +23,13 @@ int main(){
 }
 
 int add(int a, int b) {
-  return 0; // TODO : FIX IT
+  return a+b; // add
 }
 
 int sub(int a, int b) {
-  return 0; // TODO : FIX IT
+  return a-b; // sub
 }
 
 int mul(int a, int b) {
-  return 0; // TODO : FIX IT
+  return a*b; // mul
 }
